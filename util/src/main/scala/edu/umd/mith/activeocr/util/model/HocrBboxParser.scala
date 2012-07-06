fde342369a1789ed127525504d73ba86cd760fa8
Travis Brown <travisrobertbrown@gmail.com>
2012-07-06 11:24:38 -0400

    More refined character cut parsing.

--- util/src/main/scala/edu/umd/mith/activeocr/util/model/HocrBboxParser.scala
+++ util/src/main/scala/edu/umd/mith/activeocr/util/model/HocrBboxParser.scala
@@ -26,12 +26,25 @@ case class HocrBbox(
   y: Int,
   w: Int,
   h: Int,
-  items: Option[Seq[(Seq[Int], Double)]] = None
+  items: Option[List[(List[Int], Double)]] = None
 ) extends Bbox {
-  def glyphs: Option[Seq[Glyph]] = this.items.map(_.map {
-    // Unfinished: need to average cut.
-    case (cuts, _) => null
-  })
+  def glyphs(cs: Seq[String]): Option[Seq[Glyph]] = this.items.flatMap { is =>
+    if (is.size + 1 == cs.size) Some {
+      // For now we take the average x values.
+      val averaged: List[Int] = is.map {
+        case ((x :: ds), _) => x + math.round((0 :: ds).grouped(2).map {
+          case dx :: dy :: Nil => dx * (dy.toDouble / h)
+          case _ => throw new RuntimeException("Will never happen.")
+        }.sum).toInt
+        case _ => throw new RuntimeException("Will never happen.")
+      }
+      
+      (0 :: averaged).zip(averaged :+ this.rx).zip(cs).map {
+        case ((x1, x2), c) => Glyph(c, x1, y, x2 - x1, h)
+      }
+    } else None
+  }
+  def glyphs(cs: String): Option[Seq[Glyph]] = this.glyphs(cs.map(_.toString))
 }
 
 // This is a quick draft of a parser for hOCR bounding box specifications
@@ -53,17 +66,29 @@ object HocrBboxParser extends JavaTokenParsers {
     case _ => throw new RuntimeException("Will never happen.")
   }
 
-  val cuts = "cuts" ~ this.ws ~> rep1sep(this.delta, this.ws)
   val delta = rep1sep(this.integer, ",")
 
-  def nlps(cs: Seq[Seq[Int]]) =
+  val cuts = "cuts" ~ this.ws ~> rep1sep(this.delta, this.ws) ^? (
+    { case cs if cs.forall(_.size % 2 == 1) => cs.scanLeft(0 :: Nil) {
+        case (c :: _, x :: xs) => x + c :: xs
+        case _ => throw new RuntimeException("Will never happen.")
+      }.tail
+    },
+    "A cut has an even number of components: %s".format(_)
+  )
+
+  def nlps(cs: List[List[Int]]) =
     "nlp" ~> repN(cs.size, this.ws ~> this.floating) ^^ (cs.zip(_))
 
   val line = this.bbox ~ opt((this.sc ~> this.cuts) >> {
-    cs => opt(this.sc ~> this.nlps(cs)) ^^ (_.getOrElse(cs.map((_, 0.0))))
+    cs => opt(this.sc ~> this.nlps(cs)) ^^ (_ getOrElse cs.map(_ -> 0.0))
   }) ^^ {
-    case ((x1, x2), (y1, y2)) ~ items =>
-      HocrBbox(x1, y1, x2 - x1, y2 - y1, items)
+    case ((x1, y1), (x2, y2)) ~ items =>
+      HocrBbox(x1, y1, x2 - x1, y2 - y1, items.map(_.map {
+        case (cs, nlp) =>
+          val left = y2 - y1 - cs.grouped(2).map(_.lift(1).getOrElse(0)).sum
+          (cs :+ left, nlp)
+      }))
   }
 
   def apply(s: String) = this.parseAll(this.line, s.trim)
