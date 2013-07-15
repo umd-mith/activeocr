cf56b0ec6c1a67157d8fd0774ec5fc173dad1ff7
Paul Evans <decretist@gmail.com>
2013-07-15 10:36:56 -0400

    Changed ActiveOcrStep7osb.scala to StatefulSnippet. Displays SVG boxes, but does not display correct image.

--- web/src/main/scala/edu/umd/mith/activeocr/web/snippet/ActiveOcrStep7osb.scala
+++ web/src/main/scala/edu/umd/mith/activeocr/web/snippet/ActiveOcrStep7osb.scala
@@ -20,25 +20,45 @@
 package edu.umd.mith.activeocr.web {
 package snippet {
 
-import edu.umd.mith.activeocr.util.model.OcroReader
+import edu.umd.mith.activeocr.util.model.{Bbox,OcroReader}
 import java.io.File
-import net.liftweb.http.{S,SessionVar}
+import net.liftweb.http.{S,SessionVar,StatefulSnippet}
 import net.liftweb.util.Helpers._
 import scala.io.Source
 import scala.xml.NodeSeq
 import scala.xml.pull.XMLEventReader
 
-class ActiveOcrStep7osb {
+object nodesVar7osb extends SessionVar[IndexedSeq[Bbox]](IndexedSeq.empty[Bbox])
+
+class ActiveOcrStep7osb extends StatefulSnippet {
   val hocrFileName = "../data/luxmundi07multipage.html"
   val source = Source.fromFile(hocrFileName)
   val reader = new XMLEventReader(source)
   val imageFileName = "../data/luxmundi.png"
   val pages = OcroReader.parsePage(reader, new File(imageFileName).toURI)
-  val pageNumber = (S.param("pageno") map { _.toInt } openOr(0))
 
-  def transform(in: NodeSeq): NodeSeq = {
+  val lineNumber = (S.param("line") map { _.toInt } openOr(0))
+  val pageNumber = (S.param("page") map { _.toInt } openOr(0))
+  if (nodesVar7osb.is.isEmpty) {
+    nodesVar7osb(pages(pageNumber).bbList)
+  }
+  val nodes = nodesVar7osb.is
+
+  def dispatch = {
+    case "renderBottom" => renderBottom
+  }
+
+  def renderBottom(in: NodeSeq): NodeSeq = {
     <div>
-      {pages(pageNumber).toSVG}
+      <svg version="1.1"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        width="100%" height="100%"
+        viewBox="0 0 680 1149">
+        <image xlink:href="http://localhost:8080/static/images/luxmundi.png"
+          width="680" height="1149"/>
+        { nodes(this.lineNumber).toSVG }
+      </svg>
     </div>
   }
 }
