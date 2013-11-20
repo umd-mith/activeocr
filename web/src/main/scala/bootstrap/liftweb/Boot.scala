92efdf4b8cfe7715aa2972fea5f40b3d8f398b46
Paul Evans <decretist@gmail.com>
2013-11-20 10:34:15 -0500

    Minor cleanup of Boot.scala.

--- web/src/main/scala/bootstrap/liftweb/Boot.scala
+++ web/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -57,12 +57,12 @@ class Boot {
     // Build SiteMap
     def sitemap() = SiteMap(
       Menu("Home") / "index" >> User.AddUserMenusAfter, // Simple menu form
-      Menu("Active OCR Step 7.0") / "activeocr70" >> User.AddUserMenusAfter,   
-      Menu("Active OCR Step 7.1") / "activeocr71" >> User.AddUserMenusAfter,   
+      Menu("Active OCR Step 7.0") / "activeocr70" >> User.AddUserMenusAfter,
+      Menu("Active OCR Step 7.1") / "activeocr71" >> User.AddUserMenusAfter,
       Menu("Active OCR Step 7.2") / "activeocr72" >> User.AddUserMenusAfter,
-      Menu("Image Viewer") / "imageview" >> User.AddUserMenusAfter,     
+      Menu("Image Viewer") / "imageview" >> User.AddUserMenusAfter,
       Menu("Tesseract Viewer") / "tessview" >> User.AddUserMenusAfter,
-      Menu("Tesseract Editor") / "tessedit" >> User.AddUserMenusAfter,    
+      Menu("Tesseract Editor") / "tessedit" >> User.AddUserMenusAfter,
       // If you don’t add a page to your SiteMap
       // it will not be displayed by Lift on your web site.
       // Menu items marked Hidden will not be displayed in the menu hierarchy,
